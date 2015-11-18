6f313456507c4eed271196aea024d05ffad80c66
Tommy Ho <aomenlao@gmail.com>
2015-11-18 12:00:59 +0800

    Initial Commit

---
+++ PhotoCraftKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoCraftKit
+//
+//  Created by beihaiSellshou on 11/18/15.
+//  Copyright © 2015 JXHDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoCraftKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoCraftKit
+//
+//  Created by beihaiSellshou on 11/18/15.
+//  Copyright © 2015 JXHDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
